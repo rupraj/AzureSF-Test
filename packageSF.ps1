ef21ee01a67758c81eda557b89f372c941a8b108
rupraj <rupesh.rajagopalan@gmail.com>
2022-06-28 19:09:21 -0400

    Update packageSF.ps1

--- packageSF.ps1
+++ packageSF.ps1
@@ -6,6 +6,6 @@ Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-insta
 Write-Host "---- Finished Packaging project ------"
 
 Write-Host "List directory contents"
-ls "C:\windows\Microsoft.Net\Framework64"
+ls "C:\windows\Microsoft.Net\Framework64\v4.0.30319"
 
 #msbuild .\MySFWebApp8.sfproj /t:package /p:"Configuration=Release"
