06749d02e8f31277f81f3d3019b275fce48f14fa
rupraj <rupesh.rajagopalan@gmail.com>
2022-06-28 17:23:53 -0400

    Update packageSF.ps1

--- packageSF.ps1
+++ packageSF.ps1
@@ -1,7 +1,9 @@
 Write-Host "Packaging ServiceFabric project"
-msbuild .\MySFWebApp8.sfproj /t:package /p:"Configuration=Release"
+Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1';
+./dotnet-install.ps1 -InstallDir '~/.dotnet' -Version '3.1' -Runtime 'dotnetcore';
+#msbuild .\MySFWebApp8.sfproj /t:package /p:"Configuration=Release"
 
 Write-Host "---- Finished Packaging project ------"
 
 Write-Host "List directory contents"
-ls .\pkg
+ls .\.dotnet
