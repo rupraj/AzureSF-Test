c4d6d439ab92497902c3417e3d275f80c2c7c355
rupraj <rupesh.rajagopalan@gmail.com>
2022-06-28 19:16:29 -0400

    Update packageSF.ps1

--- packageSF.ps1
+++ packageSF.ps1
@@ -8,4 +8,4 @@ Write-Host "---- Finished Packaging project ------"
 Write-Host "List directory contents"
 ls "C:\windows\Microsoft.Net\Framework64\v4.0.30319"
 
-#msbuild .\MySFWebApp8.sfproj /t:package /p:"Configuration=Release"
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild\msbuild.exe .\MySFWebApp8.sfproj /t:package /p:"Configuration=Release"
