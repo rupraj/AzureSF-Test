f4d91164db6d9f693a151769242f4f2637e8dd79
rupraj <rupesh.rajagopalan@gmail.com>
2022-06-28 16:52:29 -0400

    Update installmod.ps1

--- installmod.ps1
+++ installmod.ps1
@@ -1,3 +1,10 @@
-Set-PSRepository PSGallery -InstallationPolicy Trusted
-Install-Module -Name Az.ServiceFabric
-Get-Module Az.ServiceFabric -ListAvailable
+if(Get-Module -ListAvailable -Name "Az.ServiceFabric"){
+  Write-Host "Service Fabric module exists"
+}
+else
+{
+    Set-PSRepository PSGallery -InstallationPolicy Trusted
+    Install-Module -Name Az.ServiceFabric
+    Get-Module Az.ServiceFabric -ListAvailable
+}
+
