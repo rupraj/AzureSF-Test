a7e17f032da247a4353094140c7e7f5fa1547a14
rupraj <rupesh.rajagopalan@gmail.com>
2022-07-05 21:01:56 -0400

    Update Package.ps1

--- deploy/Package.ps1
+++ deploy/Package.ps1
@@ -1,24 +1,13 @@
-﻿$env="dev"
-$envObj = Get-Content -Path .\deploy\env.json | ConvertFrom-Json | Select-Object -ExpandProperty "Environment" | Select-Object $env
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $env_name
+)
+$env_running= $env_name
 
-#create pkg folder
-New-Item .\pkg -ItemType Directory
-
-Copy-Item .\ApplicationPackageRoot\ApplicationManifest.xml -Destination .\pkg
-
-foreach($application in $envObj.$env.applicationNames){
-    New-Item .\pkg\$application"pkg" -ItemType Directory
-    New-Item .\pkg\$application"pkg"\Code -ItemType Directory
-    New-Item .\pkg\$application"pkg"\Config -ItemType Directory
-
-    Copy-Item .\$application\PackageRoot\ServiceManifest.xml -Destination .\pkg\$application"pkg"
-    Copy-Item .\$application\PackageRoot\Config\Settings.xml -Destination .\pkg\$application"pkg"\Config
-
-    dotnet publish .\$application\$application.csproj -o .\pkg\$application"pkg"\Code
+if($env_running -eq "development"){
+    Write-Host "I am in development"
+}else{
+    Write-Host "I am not in development"
 }
 
-New-Item .\artifacts -ItemType Directory
-
-Compress-Archive -Path .\pkg -DestinationPath .\artifacts\sfartifact.zip
-
-ls .\artifacts
