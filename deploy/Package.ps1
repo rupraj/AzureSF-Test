45ce8fbe0e46af6f1f81fb3d2dfbb6d1c2422fc7
rupraj <rupesh.rajagopalan@gmail.com>
2022-07-06 08:54:30 -0400

    Update Package.ps1

--- deploy/Package.ps1
+++ deploy/Package.ps1
@@ -1,36 +1,21 @@
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory = $true)]
-    [string] $env_name
-)
-if($env_name -eq "dev"){
-    Write-Host "I am in development"
-}else{
-    Write-Host "I am not in development"
-}
-
-$envObj = Get-Content -Path .\deploy\env.json | ConvertFrom-Json | Select-Object -ExpandProperty "Environment" | Select-Object $env_name
-
-
+$envObj = Get-Content -Path .\deploy\env.json | ConvertFrom-Json | Select-Object -ExpandProperty "Environment" | Select-Object "dev"
 
 New-Item .\pkg -ItemType Directory
 
 Copy-Item .\ApplicationPackageRoot\ApplicationManifest.xml -Destination .\pkg
 
 foreach($application in $envObj.$env.applicationNames){
+    New-Item .\pkg\$application"pkg" -ItemType Directory
+    New-Item .\pkg\$application"pkg"\Code -ItemType Directory
+    New-Item .\pkg\$application"pkg"\Config -ItemType Directory
 
-    Write-Host "application name" $application
-    #New-Item .\pkg\$application"pkg" -ItemType Directory
-    #New-Item .\pkg\$application"pkg"\Code -ItemType Directory
-    #New-Item .\pkg\$application"pkg"\Config -ItemType Directory
+    Copy-Item .\$application\PackageRoot\ServiceManifest.xml -Destination .\pkg\$application"pkg"
+    Copy-Item .\$application\PackageRoot\Config\Settings.xml -Destination .\pkg\$application"pkg"\Config
 
-    #Copy-Item .\$application\PackageRoot\ServiceManifest.xml -Destination .\pkg\$application"pkg"
-    #Copy-Item .\$application\PackageRoot\Config\Settings.xml -Destination .\pkg\$application"pkg"\Config
-
-    #dotnet publish .\$application\$application.csproj -o .\pkg\$application"pkg"\Code
+    dotnet publish .\$application\$application.csproj -o .\pkg\$application"pkg"\Code
 }
 
-#New-Item .\artifacts -ItemType Directory
+New-Item .\artifacts -ItemType Directory
 
-#Compress-Archive -Path .\pkg -DestinationPath .\artifacts\sfartifact.zip
+Compress-Archive -Path .\pkg -DestinationPath .\artifacts\sfartifact.zip
 
