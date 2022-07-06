b60e4b452ede7b0cec5b5134cb8a16ec0489acbb
rupraj <rupesh.rajagopalan@gmail.com>
2022-07-05 21:34:52 -0400

    Update Package.ps1

--- deploy/Package.ps1
+++ deploy/Package.ps1
@@ -5,9 +5,24 @@ param (
 )
 $env_running= $env_name
 
-if($env_running -eq "development"){
-    Write-Host "I am in development"
-}else{
-    Write-Host "I am not in development"
+$envObj = Get-Content -Path .\deploy\env.json | ConvertFrom-Json | Select-Object -ExpandProperty "Environment" | Select-Object $env_running
+
+New-Item .\pkg -ItemType Directory
+
+Copy-Item .\ApplicationPackageRoot\ApplicationManifest.xml -Destination .\pkg
+
+foreach($application in $envObj.$env.applicationNames){
+    New-Item .\pkg\$application"pkg" -ItemType Directory
+    New-Item .\pkg\$application"pkg"\Code -ItemType Directory
+    New-Item .\pkg\$application"pkg"\Config -ItemType Directory
+
+    Copy-Item .\$application\PackageRoot\ServiceManifest.xml -Destination .\pkg\$application"pkg"
+    Copy-Item .\$application\PackageRoot\Config\Settings.xml -Destination .\pkg\$application"pkg"\Config
+
+    dotnet publish .\$application\$application.csproj -o .\pkg\$application"pkg"\Code
 }
+
+New-Item .\artifacts -ItemType Directory
+
+Compress-Archive -Path .\pkg -DestinationPath .\artifacts\sfartifact.zip
 
