c07def7ccd28ffeacadcccef2847f7b1e72991b4
rupraj <rupesh.rajagopalan@gmail.com>
2022-07-06 09:11:40 -0400

    Update Package.ps1

--- deploy/Package.ps1
+++ deploy/Package.ps1
@@ -1,5 +1,7 @@
-$envObj = Get-Content -Path .\deploy\env.json | ConvertFrom-Json | Select-Object -ExpandProperty "Environment" | Select-Object "dev"
+$env="dev"
+$envObj = Get-Content -Path .\deploy\env.json | ConvertFrom-Json | Select-Object -ExpandProperty "Environment" | Select-Object $env
 
+#create pkg folder
 New-Item .\pkg -ItemType Directory
 
 Copy-Item .\ApplicationPackageRoot\ApplicationManifest.xml -Destination .\pkg
@@ -18,4 +20,6 @@ foreach($application in $envObj.$env.applicationNames){
 New-Item .\artifacts -ItemType Directory
 
 Compress-Archive -Path .\pkg -DestinationPath .\artifacts\sfartifact.zip
+
+ls .\artifacts
 
