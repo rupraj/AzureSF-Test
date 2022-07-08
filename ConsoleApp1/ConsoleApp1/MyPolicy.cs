68d0490e7a82e9a2f15c23630d45e353d2eeaa00
rupraj <rupesh.rajagopalan@gmail.com>
2022-07-07 20:16:41 -0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/MyPolicy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric.Description;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MyPolicy : UpgradePolicyDescription
+    {
+        public MyPolicy(UpgradeKind kind):base(kind)
+        {
+
+        }
+        
+        override  
+    }
+}
